9160d819a37f34a55fcc39e42a79fe7763de6125
RaphaelC <rcr@orange.fr>
2013-06-25 00:27:49 +0200

    Commented Guice Injection Singleton scope

--- src/main/java/org/jenkinsci/plugins/deploy/weblogic/support/WeblogicPluginGuiceModuleImpl.java
+++ src/main/java/org/jenkinsci/plugins/deploy/weblogic/support/WeblogicPluginGuiceModuleImpl.java
@@ -9,6 +9,7 @@ import org.jenkinsci.plugins.deploy.weblogic.task.DeploymentTaskService;
 import org.jenkinsci.plugins.deploy.weblogic.task.DeploymentTaskServiceImpl;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 
 /**
  * @author Raphael
@@ -23,6 +24,7 @@ public class WeblogicPluginGuiceModuleImpl extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(DeploymentTaskService.class).to(DeploymentTaskServiceImpl.class);
+		//.in(Singleton.class)
 	}
 
 }
