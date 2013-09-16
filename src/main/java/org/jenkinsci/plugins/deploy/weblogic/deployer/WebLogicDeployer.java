1f4feef1d8720cd41f12f6842e51d4dde98cee87
RaphaelC <rcr@orange.fr>
2013-09-16 23:35:33 +0200

    removed unused import

--- src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
+++ src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
@@ -6,10 +6,6 @@ package org.jenkinsci.plugins.deploy.weblogic.deployer;
 import hudson.model.Run.RunnerAbortedException;
 import hudson.util.ArgumentListBuilder;
 
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.deploy.weblogic.data.WebLogicAuthenticationMode;
 import org.jenkinsci.plugins.deploy.weblogic.data.WebLogicStageMode;
