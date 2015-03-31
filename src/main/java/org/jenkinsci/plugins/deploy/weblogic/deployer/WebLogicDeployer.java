9121f704c457a37f02c8e8c7512e66f60286c050
RaphC <rcr@orange.fr>
2015-03-31 20:04:36 +0200

    [JENKINS#21244] Fixed

--- src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
+++ src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
@@ -65,7 +65,7 @@ public class WebLogicDeployer {
         
         if(StringUtils.isNotBlank(parameter.getSource())) {
         	args.add("-source");
-            args.add(parameter.getSource());
+            args.add('"' +parameter.getSource()+'"');
         }
 
         args.add("-targets");
